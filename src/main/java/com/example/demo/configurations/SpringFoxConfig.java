4d90f86ed35ab4933a6d372dde68e147389496ae
ibiko <ivan.bikovic.83@gmail.com>
2020-01-20 17:40:50 +0100

    -Swagger API documentation edited

--- src/main/java/com/example/demo/configurations/SpringFoxConfig.java
+++ src/main/java/com/example/demo/configurations/SpringFoxConfig.java
@@ -4,19 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.rest.controllers"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "DEMO REST API",
+                "Spring Boot, Swagger IO, Spring REST, Hybernate, H2",
+                null,
+                "Terms of service",
+                null,
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
